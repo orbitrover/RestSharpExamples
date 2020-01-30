18bb8c48fe4ef598b8d3a823a4d1c970242f5bd1
Shailendra Singh <shailendra.singh@commdel.net>
2020-01-30 16:38:53 +0530

    dd

---
+++ RestSharpExample/RestSharpExample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RestSharpExample.Startup))]
+namespace RestSharpExample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
